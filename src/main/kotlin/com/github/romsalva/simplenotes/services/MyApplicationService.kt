ff3d684a2451eed662494f6e9f6fd74fa49c2e0b
GitHub Action <action@github.com>
2021-09-29 17:10:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/romsalva/simplenotes/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/romsalva/simplenotes/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.romsalva.simplenotes.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.romsalva.simplenotes.MyBundle
 
 class MyApplicationService {
 
